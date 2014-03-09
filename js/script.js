a78d4da3e29c5bce03b96f701e273a972cb1feb4
Graham Ludwinski <graham.ludwinski@gmail.com>
2014-03-08 19:35:23 -0500

    Version 2.0

--- js/script.js
+++ js/script.js
@@ -8,60 +8,6 @@ function scrollToLoc(location) {
 }
 
 $(document).ready(function() {
-
-	$('.fancybox').fancybox({
-		padding:0,
-		helpers : {
-			overlay : {
-				css : {
-					'background' : 'rgba(0,0,0,0.85)'
-				}
-			}
-		}
-	});
-	
-	//event handlers for responsive web design section
-	$('#phone').on('click', function() {
-		if(!$(this).hasClass('activeButton')) {
-			$(this).addClass('activeButton');
-			$('#tablet').removeClass('activeButton');
-			$('#laptop').removeClass('activeButton');
-			
-			$('#devicePreview .preview').fadeOut(100, function() {
-				$(this).removeClass('tabletPreview desktopPreview');
-				$(this).addClass('iphonePreview');
-				$(this).fadeIn(100);
-			});
-		}
-	});
-	
-	$('#tablet').on('click', function() {
-		if(!$(this).hasClass('activeButton')) {
-			$(this).addClass('activeButton');
-			$('#phone').removeClass('activeButton');
-			$('#laptop').removeClass('activeButton');
-			
-			$('#devicePreview .preview').fadeOut(100, function() {
-				$(this).removeClass('iphonePreview desktopPreview');
-				$(this).addClass('tabletPreview');
-				$(this).fadeIn(100);
-			});
-		}
-	});
-	
-	$('#laptop').on('click', function() {
-		if(!$(this).hasClass('activeButton')) {
-			$(this).addClass('activeButton');
-			$('#phone').removeClass('activeButton');
-			$('#tablet').removeClass('activeButton');
-			
-			$('#devicePreview .preview').fadeOut(100, function() {
-				$(this).removeClass('iphonePreview tabletPreview');
-				$(this).addClass('desktopPreview');
-				$(this).fadeIn(100);
-			});
-		}
-	});
 	
 	$('.contactLink').on('click', function() {
 		scrollToLoc($('#contact'));
@@ -80,8 +26,7 @@ $(document).ready(function() {
 		return false;
 	});
 	
-	/*
-$(window).scroll(function() {
+	$(window).scroll(function() {
 		if($(this).scrollTop() > 300) {
 			 $('#scrollToTop').fadeIn("fast");
 	    }
@@ -89,7 +34,6 @@ $(window).scroll(function() {
 		     $('#scrollToTop').fadeOut("fast");
 	    }
 	});
-*/
 	
 	$('#contactForm').submit(function() {
 		var contactForm = $('#contactForm');
